ef9a77cea2a20d626f4fc1bf21a4755488d712ce
Johnlyn del Castillo <johnlyndelcastillo@gmail.com>
2026-04-25 20:46:24 +0800

    implement add to home screen feature

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/src/css/style.css',
+  '/src/js/main.js',
+  '/src/js/storage.js',
+  '/src/js/tasks.js',
+  '/src/js/ui.js',
+  '/src/js/utils.js',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap',
+];
